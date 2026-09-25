8b6e0b74448bd7e318c849fe6be5ec788f59a9cb
antohaburgers <8wt4vpkbzw@privaterelay.appleid.com>
2026-09-25 15:39:38 +0500

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-// One-time retirement worker: removes the previous PWA cache and unregisters itself.
-self.addEventListener('install', event => self.skipWaiting());
-self.addEventListener('activate', event => event.waitUntil((async () => {
-  await Promise.all((await caches.keys()).map(key => caches.delete(key)));
-  await self.registration.unregister();
-  const windows = await self.clients.matchAll({ type: 'window' });
-  windows.forEach(client => client.navigate(client.url));
-})()));
