9fe0c854aee3c4c603025e67ffbd2c7131dc5a21
antohaburgers <8wt4vpkbzw@privaterelay.appleid.com>
2026-09-25 15:17:33 +0500

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE='parking-drift-v3-axles';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./style.css','./game.js','./physics.js','./input.js','./particles.js','./manifest.json']))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
